cad535c0e8aa10ccf7f00075e45b874da3d5f417
islamovahalinur9-rgb <islamovahalinur9@gmail.com>
2026-07-02 16:16:53 +0500

    Add src/hooks/useLocalStorage.ts

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+export function useLocalStorage<T>(
+  key: string,
+  initialValue: T,
+): readonly [T, Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(() => {
+    try {
+      const storedValue = window.localStorage.getItem(key);
+      return storedValue ? (JSON.parse(storedValue) as T) : initialValue;
+    } catch {
+      return initialValue;
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue] as const;
+}
